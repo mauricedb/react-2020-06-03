ee3225b0c24634ce61d1c11d6316bd8c5b13d3a4
Maurice de Beijer <maurice.de.beijer@gmail.com>
2020-06-03 18:30:08 +0200

    React router dom demo

--- mod-3-spa/src/App.js
+++ mod-3-spa/src/App.js
@@ -1,37 +1,57 @@
 import React from "react";
 import "./App.css";
 
+import { BrowserRouter, Route, Switch, Redirect, Link } from "react-router-dom";
+
 import Cat from "./components/Cat";
 import Dog from "./components/Dog";
+import notFound from "./components/404.jpg";
 
 function App() {
-  const [display, setDisplay] = React.useState("home");
-
   return (
-    <div>
-      <header>
-        <ul>
-          <li>
-            <a href="#" onClick={() => setDisplay("cat")}>
-              Cat
-            </a>
-          </li>
-          <li>
-            <a href="#" onClick={() => setDisplay("dog")}>
-              Dog
-            </a>
-          </li>
-        </ul>
-      </header>
+    <BrowserRouter>
+      <div>
+        <header>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/cat">Cat</Link>
+            </li>
+            <li>
+              <Link to="/cat/Zorro">Zorro</Link>
+            </li>
+            <li>
+              <Link to="/dog">Dog</Link>
+            </li>
+          </ul>
+        </header>
 
-      {display === "home" && (
-        <div>
-          <h2>Choose your animal</h2>
-        </div>
-      )}
-      {display === "cat" && <Cat />}
-      {display === "dog" && <Dog />}
-    </div>
+        <Switch>
+          <Route path="/cat/:catName">
+            <Cat />
+          </Route>
+          <Route path="/cat">
+            <Cat />
+          </Route>
+          <Route path="/dog">
+            <Dog />
+          </Route>
+          <Route path="/" exact>
+            <div>
+              <h2>Choose your animal</h2>
+            </div>
+          </Route>
+          <Route>
+            <Redirect to="/" />
+            {/* <div>
+              <img src={notFound} alt="No found" />
+            </div> */}
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- mod-3-spa/src/components/Cat.js
+++ mod-3-spa/src/components/Cat.js
@@ -1,10 +1,16 @@
 import React from "react";
 import imgSrc from "./cat.jpg";
 
+import { useParams, Prompt } from "react-router-dom";
+
 function Cat() {
+  const params = useParams();
+
   return (
     <div>
+      <Prompt when={true} message="Are you sure?" />
       <h2>A cute cat</h2>
+      <h3>{params.catName || "Unkown"}</h3>
 
       <img src={imgSrc} alt="A cat" height="300" />
     </div>
